ee34f41963cdf10965c1d228d3809fa5040fce5b
Srinithreddymusku <85500769+Srinithreddymusku@users.noreply.github.com>
2021-06-10 11:20:26 +0530

    Add files via upload

---
+++ bank.js
@@ -0,0 +1,26 @@
+var Bank = /** @class */ (function () {
+    function Bank(name, id, accnumber) {
+        this.custName = name;
+        this.custId = id;
+        this.custAcno = accnumber;
+    }
+    Bank.prototype.getSavingDeposit = function () {
+        return "name is:" + this.custName + " with id:" + this.custId + " and account number is: " + this.custAcno + " have amount 1000";
+    };
+    Bank.prototype.getRecurringDeposit = function () {
+        return "name is:" + this.custName + " with id:" + this.custId + " and account number is: " + this.custAcno + " have deposits 2000";
+    };
+    Bank.prototype.getFixedDeposit = function () {
+        return "name is:" + this.custName + " with id:" + this.custId + " and account number is: " + this.custAcno + " gets 10% intrest per year";
+    };
+    return Bank;
+}());
+var bank1 = new Bank('sri', 12, 1452);
+var bank2 = new Bank('nith', 45, 4712);
+var bank3 = new Bank('sai', 71, 9822);
+console.log(bank1.getSavingDeposit());
+console.log(bank2.getRecurringDeposit());
+console.log(bank1.getFixedDeposit());
+console.log(bank1.custName, bank1.custId, bank1.custAcno);
+console.log(bank2.custName, bank2.custId, bank2.custAcno);
+console.log(bank3.custName, bank3.custId, bank3.custAcno);
---
+++ function.ts
@@ -0,0 +1,11 @@
+function Bank1(custid,custname,custaccountnumber){
+    this.custid=custid 
+    this.custname=custname
+    this.custaccountnumber=custaccountnumber
+}
+var hdfc=new Bank1(101,'sri',10010)
+var sbi=new Bank1(102,'sai',11010)
+var axis=new Bank1(103,'ram',27390)
+console.log(hdfc.custid,hdfc.custname,hdfc.custaccountnumber)    
+console.log(sbi.custid,sbi.custname,sbi.custaccountnumber)  
+console.log(axis.custid,axis.custname,axis.custaccountnumber)
